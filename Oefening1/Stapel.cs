fe35229c58cd80057df3ba119e79519c90f8645f
Marialine IselonaMboyo <marialine.iselona.mboyo@student.ehb.be>
2022-10-12 21:21:06 +0200

    Starting Oef 1 >> trying some stuff to see if it works

---
+++ Oefening1/Stapel.cs
@@ -0,0 +1,82 @@
+﻿/*
+De generische klasse Stapel met volgende eigenschappen:
+De interne (private) container is gebaseerd op een List
+De publieke methoden 
+Toevoegen (iets) : zet een generisch object op de stapel
+iets Verwijderen() : haal een generisch object van de stapel en return dit aan de oproeper
+void Leegmaken() : haal alles van de stack
+string ToString () : als override van de bestaande ToString die de hele inhoud van de stack weergeeft in een string
+bool IsAanwezig (iets) : een booleaanse methode die true is als een gegeven generisch object ergens in de stack voorkomt
+Stapel Copy () : maak een "shallow" kopij van een stapel naar een nieuwe stapel
+Hint:  Zoek voor jezelf het verschil op tussen een shallow en een deep copy
+Gebruik de methode MemberwiseClone()
+ */
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oefening1
+{
+    
+    internal class Stapel<T>
+    {
+        List<T> stapel = new List<T>();
+        List<T> stapelcopy = new List<T>();
+
+        //string Appel = new string();
+        //int numbers = new int();
+
+
+        public void Toevoegen(T toevoegen)
+        {
+            stapel.Add(toevoegen);
+        }
+
+        public T Verwijderen(T verwijder)
+        {
+            stapel.Remove(verwijder);
+            return verwijder;
+        }
+
+        public void Leegmaken()
+        {
+            stapel.Clear();
+        }
+
+        public string ToString(string s)
+        {
+            s = s.Trim();
+            return s; //Elk object heeft een toString
+        }
+
+        public bool isAanwezig(T zoeken)
+        {
+            foreach (T t in stapel)
+            {
+                if (t.ToString() == zoeken.ToString())
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public List<T> listCopy()
+        {
+            stapelcopy= stapelcopy.GetRange(0, stapel.Count);
+            return stapelcopy;
+        }
+
+
+    }
+
+    //class ShallowCopy
+    //{
+
+    //}
+}
--- Oefening2/Form1.Designer.cs
+++ Oefening2/Form1.Designer.cs
@@ -28,12 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.labelAdd = new System.Windows.Forms.Label();
+            this.labelClear = new System.Windows.Forms.Label();
+            this.labelDelete = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelAdd
+            // 
+            this.labelAdd.AutoSize = true;
+            this.labelAdd.Location = new System.Drawing.Point(48, 48);
+            this.labelAdd.Name = "labelAdd";
+            this.labelAdd.Size = new System.Drawing.Size(98, 25);
+            this.labelAdd.TabIndex = 0;
+            this.labelAdd.Text = "Toevoegen";
+            // 
+            // labelClear
+            // 
+            this.labelClear.AutoSize = true;
+            this.labelClear.Location = new System.Drawing.Point(48, 213);
+            this.labelClear.Name = "labelClear";
+            this.labelClear.Size = new System.Drawing.Size(107, 25);
+            this.labelClear.TabIndex = 2;
+            this.labelClear.Text = "Leeg maken";
+            // 
+            // labelDelete
+            // 
+            this.labelDelete.AutoSize = true;
+            this.labelDelete.Location = new System.Drawing.Point(48, 128);
+            this.labelDelete.Name = "labelDelete";
+            this.labelDelete.Size = new System.Drawing.Size(103, 25);
+            this.labelDelete.TabIndex = 3;
+            this.labelDelete.Text = "Verwijderen";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelDelete);
+            this.Controls.Add(this.labelClear);
+            this.Controls.Add(this.labelAdd);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Label labelAdd;
+        private Label labelClear;
+        private Label labelDelete;
     }
 }
