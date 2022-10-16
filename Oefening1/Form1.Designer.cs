c030853b956d79462fe11f5085a77e5b0e585119
Marialine IselonaMboyo <marialine.iselona.mboyo@student.ehb.be>
2022-10-16 21:29:43 +0200

    OEF1 changes

--- Oefening1/Form1.Designer.cs
+++ Oefening1/Form1.Designer.cs
@@ -28,12 +28,222 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.btnAdd1 = new System.Windows.Forms.Button();
+            this.btnDelete1 = new System.Windows.Forms.Button();
+            this.btnClear1 = new System.Windows.Forms.Button();
+            this.btnSearch1 = new System.Windows.Forms.Button();
+            this.btnCopy1 = new System.Windows.Forms.Button();
+            this.tbStrings = new System.Windows.Forms.TextBox();
+            this.tbIntegers = new System.Windows.Forms.TextBox();
+            this.labelString = new System.Windows.Forms.Label();
+            this.labelInteger = new System.Windows.Forms.Label();
+            this.labelInt = new System.Windows.Forms.Label();
+            this.labelStr = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(84, 152);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(112, 34);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(84, 217);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(112, 34);
+            this.btnDelete.TabIndex = 1;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(84, 284);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(112, 34);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(84, 362);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(112, 34);
+            this.btnSearch.TabIndex = 3;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Location = new System.Drawing.Point(84, 442);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(112, 34);
+            this.btnCopy.TabIndex = 4;
+            this.btnCopy.Text = "Copy";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd1
+            // 
+            this.btnAdd1.Location = new System.Drawing.Point(367, 152);
+            this.btnAdd1.Name = "btnAdd1";
+            this.btnAdd1.Size = new System.Drawing.Size(112, 34);
+            this.btnAdd1.TabIndex = 5;
+            this.btnAdd1.Text = "Add";
+            this.btnAdd1.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete1
+            // 
+            this.btnDelete1.Location = new System.Drawing.Point(367, 217);
+            this.btnDelete1.Name = "btnDelete1";
+            this.btnDelete1.Size = new System.Drawing.Size(112, 34);
+            this.btnDelete1.TabIndex = 6;
+            this.btnDelete1.Text = "Delete";
+            this.btnDelete1.UseVisualStyleBackColor = true;
+            // 
+            // btnClear1
+            // 
+            this.btnClear1.Location = new System.Drawing.Point(367, 284);
+            this.btnClear1.Name = "btnClear1";
+            this.btnClear1.Size = new System.Drawing.Size(112, 34);
+            this.btnClear1.TabIndex = 7;
+            this.btnClear1.Text = "Clear";
+            this.btnClear1.UseVisualStyleBackColor = true;
+            // 
+            // btnSearch1
+            // 
+            this.btnSearch1.Location = new System.Drawing.Point(367, 362);
+            this.btnSearch1.Name = "btnSearch1";
+            this.btnSearch1.Size = new System.Drawing.Size(112, 34);
+            this.btnSearch1.TabIndex = 8;
+            this.btnSearch1.Text = "Search";
+            this.btnSearch1.UseVisualStyleBackColor = true;
+            // 
+            // btnCopy1
+            // 
+            this.btnCopy1.Location = new System.Drawing.Point(367, 442);
+            this.btnCopy1.Name = "btnCopy1";
+            this.btnCopy1.Size = new System.Drawing.Size(112, 34);
+            this.btnCopy1.TabIndex = 9;
+            this.btnCopy1.Text = "Copy";
+            this.btnCopy1.UseVisualStyleBackColor = true;
+            // 
+            // tbStrings
+            // 
+            this.tbStrings.Location = new System.Drawing.Point(63, 100);
+            this.tbStrings.Name = "tbStrings";
+            this.tbStrings.Size = new System.Drawing.Size(150, 31);
+            this.tbStrings.TabIndex = 10;
+            // 
+            // tbIntegers
+            // 
+            this.tbIntegers.Location = new System.Drawing.Point(351, 100);
+            this.tbIntegers.Name = "tbIntegers";
+            this.tbIntegers.Size = new System.Drawing.Size(150, 31);
+            this.tbIntegers.TabIndex = 11;
+            // 
+            // labelString
+            // 
+            this.labelString.AutoSize = true;
+            this.labelString.Location = new System.Drawing.Point(99, 53);
+            this.labelString.Name = "labelString";
+            this.labelString.Size = new System.Drawing.Size(82, 25);
+            this.labelString.TabIndex = 12;
+            this.labelString.Text = "STRINGS";
+            // 
+            // labelInteger
+            // 
+            this.labelInteger.AutoSize = true;
+            this.labelInteger.Location = new System.Drawing.Point(380, 53);
+            this.labelInteger.Name = "labelInteger";
+            this.labelInteger.Size = new System.Drawing.Size(90, 25);
+            this.labelInteger.TabIndex = 13;
+            this.labelInteger.Text = "INTEGERS";
+            // 
+            // labelInt
+            // 
+            this.labelInt.AutoSize = true;
+            this.labelInt.Location = new System.Drawing.Point(63, 564);
+            this.labelInt.Name = "labelInt";
+            this.labelInt.Size = new System.Drawing.Size(85, 25);
+            this.labelInt.TabIndex = 14;
+            this.labelInt.Text = "Integers: ";
+            // 
+            // labelStr
+            // 
+            this.labelStr.AutoSize = true;
+            this.labelStr.Location = new System.Drawing.Point(63, 618);
+            this.labelStr.Name = "labelStr";
+            this.labelStr.Size = new System.Drawing.Size(75, 25);
+            this.labelStr.TabIndex = 15;
+            this.labelStr.Text = "Strings: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(63, 668);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 25);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(873, 714);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.labelStr);
+            this.Controls.Add(this.labelInt);
+            this.Controls.Add(this.labelInteger);
+            this.Controls.Add(this.labelString);
+            this.Controls.Add(this.tbIntegers);
+            this.Controls.Add(this.tbStrings);
+            this.Controls.Add(this.btnCopy1);
+            this.Controls.Add(this.btnSearch1);
+            this.Controls.Add(this.btnClear1);
+            this.Controls.Add(this.btnDelete1);
+            this.Controls.Add(this.btnAdd1);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Button btnAdd;
+        private Button btnDelete;
+        private Button btnClear;
+        private Button btnSearch;
+        private Button btnCopy;
+        private Button btnAdd1;
+        private Button btnDelete1;
+        private Button btnClear1;
+        private Button btnSearch1;
+        private Button btnCopy1;
+        private TextBox tbStrings;
+        private TextBox tbIntegers;
+        private Label labelString;
+        private Label labelInteger;
+        private Label labelInt;
+        private Label labelStr;
+        private Label label3;
     }
 }
--- Oefening1/Form1.cs
+++ Oefening1/Form1.cs
@@ -6,5 +6,35 @@ namespace Oefening1
         {
             InitializeComponent();
         }
+
+        Stapel<int> integers = new Stapel<int>();
+        Stapel<string> strings = new Stapel<string>();
+
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (integers.ToString() != string.Empty)
+            {
+                integers.Verwijderen();
+            } else
+            {
+                MessageBox.Show(" It is already Empty ");
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            integers.Leegmaken();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
